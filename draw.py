4a9b19c0be2882f657198f32d88be27c81466fb1
Kjetil Valle <kjetil.valle@gmail.com>
2011-04-03 22:03:32 +0200

    setup in draw function

--- draw.py
+++ draw.py
@@ -1,17 +1,21 @@
 import rewrite
 import turtle as t
 
-def draw(sequence, step):
+def draw(sequence, position=(0,0), heading=0, step_length=30, angle_increment=60):
+    t.up()
+    t.goto(position)
+    t.right(heading)
+    t.down()
     for s in sequence:
-        if s is 'f': t.forward(step)
-        if s is '+': t.right(60)
-        if s is '-': t.left(60)
+        if s is 'f': t.forward(step_length)
+        if s is '+': t.right(angle_increment)
+        if s is '-': t.left(angle_increment)
 
 def demo_koch():
     level = 3
     fractal = rewrite.koch_fractal(level)
     step = 300/(3**level)
-    draw(fractal, step)
+    draw(fractal, position=(-200,0),step_length=step, angle_increment=60)
 
 if __name__=="__main__":
     demo_koch()
