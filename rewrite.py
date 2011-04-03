4d358c25b890e125fd83c4fba6a3a828df52ada1
Kjetil Valle <kjetil.valle@gmail.com>
2011-04-03 19:52:24 +0200

    rewrite function

---
+++ rewrite.py
@@ -0,0 +1,29 @@
+def rewrite(axiom, rules):
+    i = 0
+    while i < len(axiom):
+        x = axiom[i]
+        if x in rules.keys():
+            index = i + axiom[i:].index(x)
+            axiom = axiom[:index]+rules[x]+axiom[index+1:]
+            i += len(rules[x])
+            continue
+        i += 1
+    return axiom
+
+def test_rewrite():
+    rules = {'a': 'ab',
+             'b': 'c'}
+    assert('ab'==rewrite('a', rules))
+    assert('abc'==rewrite('ab', rules))
+    rules = {'f': 'f+f--f+f'}
+    s = rewrite('f', rules)
+    s = rewrite(s, rules)
+    t = 'f+f--f+f+f+f--f+f--f+f--f+f+f+f--f+f'
+    assert(t==s)
+    print 'ok'
+
+def run_tests():
+    test_rewrite()
+
+if __name__=="__main__":
+    run_tests()
