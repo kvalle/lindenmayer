d81d18346c00438c178f5b946e8f1637e53a2634
Kjetil Valle <kjetil.valle@gmail.com>
2011-04-03 22:58:13 +0200

    Turtle class

--- draw.py
+++ draw.py
@@ -1,37 +1,51 @@
 import rewrite
 import turtle as t
 
-def draw(sequence, position=(0,0), heading=0, step_length=30, angle_increment=60):
-    t.up()
-    t.goto(position)
-    t.right(heading)
-    t.down()
-    for s in sequence:
-        if s is 'F':
-            t.forward(step_length)
-        elif s is 'f':
-            t.up()
-            t.forward(step_length)
-            t.down()
-        elif s is '+':
-            t.left(angle_increment)
-        elif s is '-':
-            t.right(angle_increment)
-        else:
-            print "Symbol '"+ s +"' ignored because it was not recognized by the turtle!'"
+class Turtle():
+    def __init__(self, position=(0,0), heading=0, step=30, angle=60):
+        self.position = position
+        self.heading = heading
+        self.step = step
+        self.angle = angle
+        self.setup()
+
+    def setup(self):
+        t.clear()
+        t.up()
+        t.goto(self.position)
+        t.right(self.heading)
+        t.down()
+
+    def draw(self, sequence):
+        for s in sequence:
+            if s is 'F':
+                t.forward(self.step)
+            elif s is 'f':
+                t.up()
+                t.forward(self.step)
+                t.down()
+            elif s is '+':
+                t.left(self.angle)
+            elif s is '-':
+                t.right(self.angle)
+            else:
+                print "Symbol '"+ s +"' ignored because it was not recognized by the turtle!'"
 
 def demo_koch(level = 3):
     fractal = rewrite.koch_fractal(level)
     step = 300/(3**level)
-    draw(fractal, position=(-200,0),step_length=step, angle_increment=60)
+    koopa = Turtle(position=(-200,0),step=step, angle=60)
+    koopa.draw(fractal)
     raw_input('Press return to end')
 
 def demo_quadratic_koch(level=2):
     t.speed(10)
     fractal = rewrite.quadratic_koch_fractal(level)
     step = 100/(5*level+1)
-    draw(fractal,step_length=step, angle_increment=90)
+    koopa = Turtle(step=step, angle=90)
+    koopa.draw(fractal)
     raw_input('Press return to end')
 
 if __name__=="__main__":
+    demo_koch()
     demo_quadratic_koch()
