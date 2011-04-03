7e3d28ae60fad2ff7617dd9ca0a1db799a68efd4
Kjetil Valle <kjetil.valle@gmail.com>
2011-04-03 22:17:15 +0200

    changes in draw()

--- draw.py
+++ draw.py
@@ -7,16 +7,25 @@ def draw(sequence, position=(0,0), heading=0, step_length=30, angle_increment=60
     t.right(heading)
     t.down()
     for s in sequence:
-        if s is 'f': t.forward(step_length)
-        if s is '+': t.right(angle_increment)
-        if s is '-': t.left(angle_increment)
+        if s is 'F':
+            t.forward(step_length)
+        elif s is 'f':
+            t.up()
+            t.forward(step_length)
+            t.down()
+        elif s is '+':
+            t.left(angle_increment)
+        elif s is '-':
+            t.right(angle_increment)
+        else:
+            print "Symbol '"+ s +"' ignored because it was not recognized by the turtle!'"
 
 def demo_koch():
     level = 3
     fractal = rewrite.koch_fractal(level)
     step = 300/(3**level)
     draw(fractal, position=(-200,0),step_length=step, angle_increment=60)
+    raw_input('Press return to end')
 
 if __name__=="__main__":
     demo_koch()
-    raw_input('Press return to end')
