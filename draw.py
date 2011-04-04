b0c2a98d7483cd86e149e37dd1187fe3104c2755
Kjetil Valle <kjetil.valle@gmail.com>
2011-04-04 09:40:21 +0200

    added draw()'s char mapping

--- draw.py
+++ draw.py
@@ -12,12 +12,12 @@ class KoopaTroopa(t.Turtle):
         self.right(heading)
         self.down()
 
-    def save_state(self):
+    def mark(self):
         p = self.position()
         h = self.heading()
         self._states.append((p,h))
 
-    def restore_state(self):
+    def recall(self):
         if not self._states:
             raise Exception('No states to restore from!')
         p,h = self._states[-1]
@@ -27,22 +27,24 @@ class KoopaTroopa(t.Turtle):
         self.down()
         self._states = self._states[:-1]
 
+    def forward_up(step):
+        self.up()
+        self.forward(step)
+        self.down()
+
     def draw(self, sequence):
+        mapping = {
+                'F':(self.forward, [self._step]),
+                'f':(self.forward_up, [self._step]),
+                '+':(self.left, [self._angle]),
+                '-':(self.right, [self._angle]),
+                '[':(self.mark, []),
+                ']':(self.recall, [])
+            }
         for s in sequence:
-            if s is 'F':
-                self.forward(self._step)
-            elif s is 'f':
-                self.up()
-                self.forward(self._step)
-                self.down()
-            elif s is '+':
-                self.left(self._angle)
-            elif s is '-':
-                self.right(self._angle)
-            elif s is '[':
-                self.save_state()
-            elif s  is ']':
-                self.restore_state()
+            if s in mapping.keys():
+                fun, args = mapping[s]
+                fun(*args)
             else:
                 print "Symbol '"+ s +"' ignored because it was not recognized by the turtle!"
 
@@ -71,11 +73,11 @@ def demo_tree():
     raw_input('Press return to end')
 
 def demo_triangles():
-    axiom = 'a'
-    rules = {'a': 'FaFbF-', 'b':'-FaFbF'}
+    axiom = 'l'
+    rules = {'l': 'FlFrF-', 'r':'-FlFrF'}
     tree = rewrite.rewrite_iter(axiom, rules, 4)
-    tree = tree.replace('a','+')
-    tree = tree.replace('b','+')
+    tree = tree.replace('l','+')
+    tree = tree.replace('r','+')
     koopa = KoopaTroopa(position=(0,250),step=100, angle=120, heading=120)
     koopa.draw(tree)
     raw_input('Press return to end')
