2567459283329acbc0bfa64207041ff894ab797c
Kjetil Valle <kjetil.valle@bekk.no>
2012-02-23 22:48:46 +0100

    moved call to getattr ++

--- demo.py
+++ demo.py
@@ -7,52 +7,57 @@ class Demo:
 
     def __init__(self):
         turtle.bgcolor('black')
+
+    def _list_demos(self):
+        return [method for method in dir(self) if not method[0]=="_"]
         
-    def _draw(self):
-        self.koopa.width(2)
-        self.koopa.color('white')
-        self.koopa.speed(8)
-        self.koopa.draw(self.path)
+    def _draw(self, demo, levels):
+        fun = getattr(self,demo)
+        koopa, path = fun(levels) if levels else fun()
+        koopa.width(2)
+        koopa.color('white')
+        koopa.speed(8)
+        koopa.draw(path)
         raw_input()
 
     def koch(self, levels = 3):
-        self.path = systems.koch_fractal(levels)
-        self.koopa = draw.KoopaTroopa(position=(-200,0), step=300/(3**level), angle=60)
-        self._draw()
+        path = systems.koch_fractal(levels)
+        koopa = draw.KoopaTroopa(position=(-200,0), step=300/(3**levels), angle=60)
+        return koopa, path
 
     def koch_quad(self, levels=2):
-        self.path = systems.quadratic_koch_fractal(levels)
-        self.koopa = draw.KoopaTroopa(step=100/(5*level+1), angle=90)
-        self._draw()
+        path = systems.quadratic_koch_fractal(levels)
+        koopa = draw.KoopaTroopa(step=100/(5*levels+1), angle=90)
+        return koopa, path
 
     def tree(self, levels=3):
-        self.path = systems.tree(levels)
-        self.koopa = draw.KoopaTroopa(position=(0,-250), step=12, angle=29, heading=270)
-        self._draw()
+        path = systems.tree(levels)
+        koopa = draw.KoopaTroopa(position=(0,-250), step=12, angle=29, heading=270)
+        return koopa, path
 
     def tree2(self, levels=3):
-        self.path = systems.tree2(levels)
-        self.koopa = draw.KoopaTroopa(position=(0,-250), step=10, angle=29, heading=270)
-        self._draw()
+        path = systems.tree2(levels)
+        koopa = draw.KoopaTroopa(position=(0,-250), step=10, angle=29, heading=270)
+        return koopa, path
 
     def triangles(self, levels=4):
-        self.path = systems.triangles(levels)
-        self.koopa = draw.KoopaTroopa(position=(0,250), step=100, angle=120, heading=120)
-        self.koopa.add_mappings({'l':'left', 'r':'left'})
-        self._draw()
+        path = systems.triangles(levels)
+        koopa = draw.KoopaTroopa(position=(0,250), step=100, angle=120, heading=120)
+        koopa.add_mappings({'l':'left', 'r':'left'})
+        return koopa, path
 
     def sierpinski(self, levels=6):
-        self.path = systems.sierpinski(levels)
-        self.koopa = draw.KoopaTroopa(position=(-200,-200), step=7)
-        self.koopa.add_mappings({'a':'draw', 'b':'draw'})
-        self._draw()
+        path = systems.sierpinski(levels)
+        koopa = draw.KoopaTroopa(position=(-200,-200), step=7)
+        koopa.add_mappings({'a':'draw', 'b':'draw'})
+        return koopa, path
 
     def shrooms(self, levels=6):
-        self.path = systems.shroom_fractal(levels)
-        self.koopa = draw.KoopaTroopa(position=(-360,-150), step=3, heading=330)
-        self.koopa.add_mappings({'a':'draw', 'b':'draw'})
-        self.koopa.hideturtle()
-        self._draw()
+        path = systems.shroom_fractal(levels)
+        koopa = draw.KoopaTroopa(position=(-360,-150), step=3, heading=330)
+        koopa.add_mappings({'a':'draw', 'b':'draw'})
+        koopa.hideturtle()
+        return koopa, path
     
 if __name__=='__main__':
     import sys
@@ -61,33 +66,21 @@ if __name__=='__main__':
         print "Usage:"
         print "  python demo.py DEMO [LEVEL]"
         print
-        print "DEMO\t{" + ", ".join(demos)+"}"
+        print "DEMO\t{" + ", ".join(Demo()._list_demos())+"}"
         print "LEVEL\tSpecify number of iterations for rewrite-engine"
         sys.exit()
     
-    def get_demo_method():    
+    def level(arg):
         try:
-            return getattr(Demo(),sys.argv[1])
-        except AttributeError:
-            print "! Specified DEMO not one of: " + ", ".join(demos)
-            sys.exit() 
-            
-    def get_level():
-        try:
-            level = int(sys.argv[2])
+            level = int(arg)
             assert level > 0
             return level
         except:
             print "! LEVEL must be positive integer"
             sys.exit() 
         
-    demos = [method for method in dir(Demo) if not method[0]=="_"]
     if len(sys.argv) == 1:
         print_usage_and_die()
-    demo = get_demo_method()
-    if len(sys.argv) > 2:
-        level = get_level()
-        demo(level)
-    else:
-        demo()
+    level = level(sys.argv[2]) if len(sys.argv) > 2 else None
+    Demo()._draw(sys.argv[1], level)
     
